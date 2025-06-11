a63ebe987c3dde54d5a023c812a9744b58cdac91
Solange Aurrecoechea <solangeaurrecoechea@gmail.com>
2025-06-11 17:08:47 +0200

    Update and rename train (5).py to train.py

---
+++ train_fly_model/runs/run01/train.py
@@ -0,0 +1,76 @@
+#%%
+import yaml
+import torch
+import logging
+from fly_organelles.run import run
+
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True
+    # torch.backends.cudnn.benchmark = True
+
+logger = logging.getLogger(__name__)
+
+log_dir = "/nrs/cellmap/zouinkhim/tensorboard/panc_lsds"
+
+voxel_size = (16, 16, 16)
+l_rate = 0.5e-5
+batch_size = 14
+
+affinities_map = [
+                        [1, 0, 0],
+                        [0, 1, 0],
+                        [0, 0, 1],
+    ]
+
+
+N_AFFINITIES = len(affinities_map)
+
+#%%
+#%%
+labels = [f"lsd_{i}" for i in range(10)]+[f"aff_{i}" for i in range(3)]
+
+
+#%%
+
+
+yaml_file = "/groups/cellmap/cellmap/zouinkhim/salevary/yamls/datasets_generated.yaml"
+iterations = 1000000
+
+
+
+with open(yaml_file, "r") as data_yaml:
+    datasets = yaml.safe_load(data_yaml)
+
+
+from isolated_unet import UNetLSD, load_checkpoint_from_path
+
+model = UNetLSD()
+status = load_checkpoint_from_path(model, "/groups/cellmap/cellmap/zouinkhim/lsds_work/398151")
+print(status)  # Should print "<All keys matched successfully>"
+
+x = torch.rand((1, 1, 196, 196, 196))
+y = model(x)
+print(y.shape)
+
+import gunpowder as gp
+input_size = gp.Coordinate( 196, 196, 196)
+output_size = gp.Coordinate(92, 92, 92)
+
+
+model = model.to(device)
+run(model,
+iterations, 
+labels, 
+None, 
+datasets,
+voxel_size = voxel_size,
+batch_size = batch_size, 
+l_rate=l_rate,
+log_dir=log_dir,
+affinities = True, 
+affinities_map = affinities_map,
+)
