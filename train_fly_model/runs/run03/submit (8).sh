e10194d3e5a7db3d6ca9fbefa2b834f6926d1af1
Solange Aurrecoechea <solangeaurrecoechea@gmail.com>
2025-12-02 09:22:56 +0100

    Update submit (8).sh

--- train_fly_model/runs/run03/submit (8).sh
+++ train_fly_model/runs/run03/submit (8).sh
@@ -1,1 +1,1 @@
-bsub -J "innervationrun02" -P cellmap -n 12 -q gpu_h100 -gpu "num=1" -o output.log -e error.log python train.py
+bsub -J "innervationrunnoMyelinated" -P cellmap -n 12 -q gpu_h100 -gpu "num=1" -o output.log -e error.log python train.py
